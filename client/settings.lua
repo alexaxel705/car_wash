593d16314fd04760f4a5428ff4c7b9ed43dbddfb
alexaxel705 <alexaxel705@gmail.com>
2020-02-05 04:03:54 +0700

    commit

--- client/settings.lua
+++ client/settings.lua
@@ -19,27 +19,13 @@ dirtLevel = {
 -- координаты моек
 clearStation = {
 	{2360, -657, 128},
+	{1911.6395263672, -1775.4776611328, 12.900556564331}, -- Idlewood, Los Santos, at the Going station
+	{2454.3850097656, -1460.5235595703, 23.419036865234}, -- East Los Santos, northeast of Cluckin' Bell 
+	{1017.7843017578, -917.67077636719, 42.1796875}, -- South Mulholland, Los Santos, behind the Quite Humorous Comedy Club
+	{2163.1999511719, 2472.99609375, 10.8203125}, -- Soapys Car Wash, Roca Escalante, Las Venturas
 }
 
--- Idlewood, Los Santos, at the Going station
-clearStation = {
-	{1911.6395263672, -1775.4776611328, 12.900556564331},
-}
 
--- East Los Santos, northeast of Cluckin' Bell 
-clearStation = {
-	{2454.3850097656, -1460.5235595703, 23.419036865234},
-}
-
--- South Mulholland, Los Santos, behind the Quite Humorous Comedy Club
-clearStation = {
-	{1017.7843017578, -917.67077636719, 42.1796875},
-}
-
--- Soapys Car Wash, Roca Escalante, Las Venturas
-clearStation = {
-	{2163.1999511719, 2472.99609375, 10.8203125},
-}
 
 -- названия текстур на которые накладывается грязь
 textureName = {
